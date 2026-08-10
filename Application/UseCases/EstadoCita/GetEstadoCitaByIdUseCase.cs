2cbaf4e7f34306b974292de82473a58f8e47b76c
Sahiam1810 <222233860+Sahiam1810@users.noreply.github.com>
2026-08-10 12:11:08 -0500

    feat: :sparkles: Agregar Archivos para cada caso de uso
    Agregar Archivos para cada caso de uso

---
+++ Application/UseCases/EstadoCita/GetEstadoCitaByIdUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.UseCases.EstadoCita
+{
+    internal class GetEstadoCitaByIdUseCase
+    {
+    }
+}
---
+++ Application/UseCases/Horarios/GetAllHorarioUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.UseCases.Horarios
+{
+    internal class GetAllHorarioUseCase
+    {
+    }
+}
