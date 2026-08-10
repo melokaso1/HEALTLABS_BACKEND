9bc63b946a013ac3c6d9efe4af738a70f8280161
Alejandro escobar <107068494+melokaso1@users.noreply.github.com>
2026-08-10 11:26:06 -0500

    refactor: :fire: Remove unused Class1 and Interface1 files from Domain

---
+++ Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        public Task<TEntity> AddAsync(TEntity entity);
+        public Task<TEntity> UpdateAsync(TEntity entity);
+        public Task DeleteAsync(TEntity entity);
+        public Task<TEntity> GetEntityByIdAsync();
+        public Task<IEnumerable<TEntity>> GetAllEntitiesAsync();
+    }
+}
