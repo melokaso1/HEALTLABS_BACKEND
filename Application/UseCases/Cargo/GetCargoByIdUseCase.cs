9f5626362de3d5a613b2c9f31d5bf873aa7ca172
Sahiam1810 <222233860+Sahiam1810@users.noreply.github.com>
2026-08-11 06:54:33 -0500

    feat: caso de usos para cargo
    caso de usos para cargo

--- Application/UseCases/Cargo/GetCargoByIdUseCase.cs
+++ Application/UseCases/Cargo/GetCargoByIdUseCase.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Entities;
+using Domain.Interfaces;
 
 namespace Application.UseCases.Cargo
 {
-    internal class GetCargoByIdUseCase
+    public class GetCargoByIdUseCase
     {
+        private readonly IGenericRepository<CargoEntity> _repo;
+
+        public GetCargoByIdUseCase(IGenericRepository<CargoEntity> repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<CargoEntity> ExecuteAsync(Guid id)
+        {
+            var entity = await _repo.GetEntityByIdAsync(id);
+            return entity;
+        }
     }
 }
